88b74d7f9ea516669db8a877a2b9e06933377700
hana <asufanah@gmail.com>
2015-07-01 22:41:36 +0900

    Exercise 2.2 and List 2.6

--- src/test/scala/c2_6/C2_6Test.scala
+++ src/test/scala/c2_6/C2_6Test.scala
@@ -4,10 +4,12 @@ import org.scalatest.{FunSuite, ShouldMatchers}
 
 class C2_6Test extends FunSuite with ShouldMatchers {
 
+    def filterByLength(s:String): Boolean = s.length() > 2
+
     test("c2_6"){
-        val i: Int = C2_6.findFirst[String](Array("a","bb","ccc","dddd"), C2_6
-            .filterByString)
-        assert(i == 3)
+        assert(C2_6.findFirst[String](Array("a","bb","ccc"),  filterByLength) == 2)
+
+        assert(C2_6.findFirst[String](Array("a","bb","ccc"),  s => s.length() > 2) == 2)
     }
 
 }
